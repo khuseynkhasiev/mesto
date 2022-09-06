1d3f325400566f5831db6b7cb6e6aec681c81a6f
Khuseyn Khasiev <khuseyn-khasiev@mail.ru>
2022-09-06 11:23:06 +0300

    fix: убрать лишний код js

--- index.js
+++ index.js
@@ -9,27 +9,6 @@ const profileEditButton = document.querySelector('.profile__edit-button');
 const profileTitle = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__job');
 
-
-
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value  
-    profileTitle.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
-}
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler); 
-
-
-
 // функция открытия и закрытия popup
 function popupToggle() {
    popup.classList.toggle('popup_opened');
@@ -48,11 +27,11 @@ popupClose.addEventListener('click', () =>{
 });
 
 // сохранение внесенных изменений в popup
-/* popupSaveButton.addEventListener('click', () =>{
+popupSaveButton.addEventListener('click', () =>{
     profileTitle.innerText = nameInput.value;
     profileJob.innerText = jobInput.value;
     popupToggle();
-}); */
+});
 
 const elLike = document.querySelectorAll('.elements__like');
 // реализация лайка для фотографий
@@ -61,15 +40,3 @@ for (let i = 0; i < elLike.length; i++) {
         elLike[i].classList.toggle('elements__like_active');
     });
 }
-
-/* Обрезаем строку больше 12 символов
-const elTitle = document.querySelectorAll('.elements__title');
-let elTitleStr = '';
-for (let i = 0; i < elTitle.length; i++) {
-    elTitleStr = elTitle[i].textContent;
-
-    if (elTitleStr.length > 12){
-        console.log(elTitleStr);
-        elTitle[i].textContent = elTitleStr.slice(0, 11) + '...';
-    }
-} */
