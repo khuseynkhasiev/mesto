3d34ac4d25796675a031bf5797937db7d01ee254
Khuseyn Khasiev <khuseyn-khasiev@mail.ru>
2022-09-06 12:03:50 +0300

    fix: переделать кнопку сохранить под требования к ПР

--- index.js
+++ index.js
@@ -5,9 +5,28 @@ const nameInput = formElement.querySelector('.popup__name-input');
 const jobInput = formElement.querySelector('.popup__job-input');
 const popupSaveButton = formElement.querySelector('.popup__save-btn');
 
-const profileEditButton = document.querySelector('.profile__edit-button');
-const profileTitle = document.querySelector('.profile__title');
-const profileJob = document.querySelector('.profile__job');
+const profile = document.querySelector('.profile');
+const profileEditButton = profile.querySelector('.profile__edit-button');
+const profileTitle = profile.querySelector('.profile__title');
+const profileJob = profile.querySelector('.profile__job');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+        profileTitle.textContent = nameInput.value;
+        profileJob.textContent = jobInput.value;
+    // Выберите элементы, куда должны быть вставлены значения полей
+    // Вставьте новые значения с помощью textContent
+    // закрытие попапа после сохранения
+    popupToggle();
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler); 
 
 // функция открытия и закрытия popup
 function popupToggle() {
@@ -16,20 +35,13 @@ function popupToggle() {
 
 //открытие popup редактирования с сохранением новых значений
 profileEditButton.addEventListener('click', () =>{
-    nameInput.value = profileTitle.innerText;
-    jobInput.value = profileJob.innerText;    
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileJob.textContent;    
     popupToggle();
 });
 
 //закрытие popup
 popupClose.addEventListener('click', () =>{
-    popupToggle();
-});
-
-// сохранение внесенных изменений в popup
-popupSaveButton.addEventListener('click', () =>{
-    profileTitle.innerText = nameInput.value;
-    profileJob.innerText = jobInput.value;
     popupToggle();
 });
 
